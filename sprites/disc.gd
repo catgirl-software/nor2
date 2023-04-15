d55ecea8ed05c0ff9b3bc3fbe5ef262711013fc8
elle <elle@catgirl.software>
2023-04-15 11:37:09 +0930

    discs

--- sprites/disc.gd
+++ sprites/disc.gd
@@ -1,19 +1,23 @@
-extends RigidBody2D
+extends CharacterBody2D
 class_name Disc
 
-const ACCELERATION : float = 0.4 * 60
-const MAX_VEL : int = 20
+const ACCELERATION : float = 2
+const MAX_VEL : int = 100
 const MAX_VEL_SQUARED : int = MAX_VEL * MAX_VEL
 
-func _ready():
-	apply_central_force(Vector2(0,600))
-	
-func accelerate_slightly(_body):
-	apply_central_force(linear_velocity * ACCELERATION)
-	print("Collision! vel: " + str(linear_velocity.length()))
+var vel: Vector2
 
-func _integrate_forces(state):
-	if state.linear_velocity.length_squared() > MAX_VEL_SQUARED:
-		print("Too fast? length squared: " + str(state.linear_velocity.length_squared()))
-		state.linear_velocity = state.linear_velocity.normalized() * MAX_VEL
-		print("Dropped speed! vel: " + str(linear_velocity) + ", len: " + str(linear_velocity.length()) )
+func _ready():
+	self.vel = Vector2(10, 10)
+
+func _physics_process(delta):
+	var movement = self.vel
+	while true:
+		var col = self.move_and_collide(movement * delta)
+		if col:
+			print("bounce! ", self.vel)
+			self.vel = self.vel.bounce(col.get_normal()) * ACCELERATION
+			self.vel = self.vel.normalized() * min(MAX_VEL, self.vel.length())
+			movement = col.get_remainder().bounce(col.get_normal())
+		else:
+			break
