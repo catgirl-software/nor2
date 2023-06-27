d22ab9ba1380cd2a72ab30cb877ad0fc0a59cfcd
emmawithanE <emma@catgirl.software>
2023-06-28 01:36:40 +0930

    Make intermission ui more clear maybe? seek input. Add PanelTheme, which can be used for team coloured (or neutral) panels.

--- scenes/component_scenes/Int_ScoreBox.gd
+++ scenes/component_scenes/Int_ScoreBox.gd
@@ -1,18 +1,30 @@
-extends HBoxContainer
+extends PanelContainer
 class_name Int_ScoreBox
 
 @export var scoring_sprite : Texture2D
 
+var score_disc_scene : PackedScene = preload("res://scenes/component_scenes/ScoreDisc.tscn")
+
 func init(player_name : String, scores : Array):
-	$CenterContainer/NameLabel.text = player_name
+	
+	# Let's be colourful
+	var theme_name : String = "PanelContainer"
+	match player_name:
+		"Player 1": theme_name += "Green"
+		"Player 2": theme_name += "Orange"
+		"Player 3": theme_name += "Blue"
+		"Player 4": theme_name += "Purple"
+		_:
+			print("Unexpected player_name: ", player_name)
+	
+	print("Setting theme variation to " + theme_name)
+	set_theme_type_variation(theme_name)
+	
+	$HBoxContainer/CenterContainer/NameLabel.text = player_name
+	$HBoxContainer/CenterContainer/KillsLabel.text = "Kills: " + str(len(scores))
 	print("Init scorebox for player ", player_name, " scores:", scores)
 	for score in scores:
 		print("loop!")
-		var new_node = Sprite2D.new()
-		new_node.texture = scoring_sprite
-		new_node.vframes = 5
-		new_node.frame = score
-		new_node.scale = Vector2(2,2)
-		var int_child = Control.new()
-		int_child.add_child(new_node)
-		add_child(int_child)
+		var new_node = score_disc_scene.instantiate()
+		new_node.init(score)
+		$HBoxContainer/ScoreContainer.add_child(new_node)
