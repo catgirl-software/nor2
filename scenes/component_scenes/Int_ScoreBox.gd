f04a86748bb58d0df4a98ebf894090dbb88f78f7
emma <emma@catgirl.software>
2023-07-22 09:39:47 +0930

    Better score UI maybe

--- scenes/component_scenes/Int_ScoreBox.gd
+++ scenes/component_scenes/Int_ScoreBox.gd
@@ -1,24 +1,35 @@
 extends PanelContainer
 class_name Int_ScoreBox
 
-@export var scoring_sprite : Texture2D
+#@export var scoring_sprites : Array[Texture2D]
 
 var score_disc_scene : PackedScene = preload("res://scenes/component_scenes/ScoreDisc.tscn")
 
 func init(player_name : String, scores : Array):
 	
+	var index: int
 	# Let's be colourful
 	var theme_name : String = "PanelContainer"
 	match player_name:
-		"Player 1": theme_name += "Green"
-		"Player 2": theme_name += "Orange"
-		"Player 3": theme_name += "Blue"
-		"Player 4": theme_name += "Purple"
+		"Player 1": 
+			theme_name += "Green"
+			index = 0
+		"Player 2": 
+			theme_name += "Orange"
+			index = 1
+		"Player 3": 
+			theme_name += "Blue"
+			index = 2
+		"Player 4": 
+			theme_name += "Purple"
+			index = 3
 		_:
 			print("Unexpected player_name: ", player_name)
 	
 	print("Setting theme variation to " + theme_name)
 	set_theme_type_variation(theme_name)
+	
+	$HBoxContainer/PanelContainer.self_modulate = TeamSettings.team_colours[index]
 	
 	$HBoxContainer/CenterContainer/NameLabel.text = player_name
 	$HBoxContainer/CenterContainer/KillsLabel.text = "Kills: " + str(len(scores))
@@ -27,4 +38,4 @@ func init(player_name : String, scores : Array):
 		print("loop!")
 		var new_node = score_disc_scene.instantiate()
 		new_node.init(score)
-		$HBoxContainer/ScoreContainer.add_child(new_node)
+		$HBoxContainer/PanelContainer/ScoreContainer.add_child(new_node)
